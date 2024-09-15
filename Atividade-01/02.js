1a8a5852b68bfa40707781e5cf65fdb65e2fb342
Plinio <plinioricardogoncalves@gmail.com>
2024-09-15 12:12:37 -0300

    atividade 01 - questao 02

---
+++ Atividade-01/02.js
@@ -0,0 +1,9 @@
+/*
+2. Faça um algoritmo para calcular quantas ferraduras são necessárias para equipar todos os cavalos comprados para um haras.
+*/
+
+let cavalos = parseInt(prompt("Quantos cavalos existem no haras?"));
+
+let ferraduras = cavalos * 4;
+
+alert(`Será necessário comprar ${ferraduras} ferraduras.`);
