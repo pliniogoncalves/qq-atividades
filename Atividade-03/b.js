5d35cae6ac087b9de043aecb0c04033c104a3203
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-16 17:35:04 -0300

    atividade 03 - questao b

--- Atividade-03/b.js
+++ Atividade-03/b.js
@@ -2,9 +2,10 @@
 b. Ler uma temperatura em graus Fahrenheit e apresentá-la convertida em graus Celsius. A fórmula de
 conversão é C ← (F -32)* (5/9), sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
 */
+function convertCelsius(){
+    let fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
 
-let fahrenheit = parseFloat(prompt("Digite a temperatura em Fahrenheit"));
+    let celsius = (fahrenheit - 32) * (5/9) ;
 
-let celsius = (fahrenheit - 32) * (5/9) ;
-
-alert(`A temperatura em Fahrenheit ${fahrenheit} convertida em Celsius é ${celsius}`);
+    document.getElementById("resultado").innerText = `A temperatura em Fahrenheit ${fahrenheit}°F convertida em Celsius é ${celsius}°C`;
+}
