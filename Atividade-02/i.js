93520bc81e83f83c6d7e18a3dcfd8e3c9d9c87b8
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-16 09:43:37 -0300

    atividade 02 - questao i

---
+++ Atividade-02/i.js
@@ -0,0 +1,10 @@
+/*
+i.Efetuar a leitura de um número inteiro e apresentar o resultado do quadrado
+desse número. 
+*/
+
+let numero = parseInt(prompt("Digite o valor do numero inteiro"));
+
+let quadrado = numero * numero ;
+
+alert(`O quadrado de ${numero} é ${quadrado}`);
