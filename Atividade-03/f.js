bea4d2f5ce33f73d553a17c08345dc72ed1ff3c4
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-16 19:02:29 -0300

    atividade 03 - questao f

--- Atividade-03/f.js
+++ Atividade-03/f.js
@@ -4,12 +4,15 @@ forma que a variável A passe a possuir o valor da variável B e a variável B
 passe a possuir o valor da variável A. Apresentar os valores trocados. 
 */
 
-let a = parseFloat(prompt("Digite o valor da Variável A"));
-let b = parseFloat(prompt("Digite o valor da variável B"));
+function trocarValores(){
+    let a = parseFloat(document.getElementById("a").value);
+    let b = parseFloat(document.getElementById("b").value);
 
-console.log(a,b);
-let c = a;
-a = b;
-b = c;
-console.log(a,b);
-alert(`O valor de A agora é ${a} e o valor de b agora é ${b}`);
+    console.log(a,b);
+    let c = a;
+    a = b;
+    b = c;
+    console.log(a,b);
+
+    document.getElementById("resultado").innerText = `O valor de A agora é ${a} e o valor de B agora é ${b}`;
+}
