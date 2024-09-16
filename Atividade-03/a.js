56ad1e9922b85ee773b5301fcec57c0e463437bc
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-16 12:00:51 -0300

    atividade 03 estrutura base

--- Atividade-03/a.js
+++ Atividade-03/a.js
@@ -1,4 +1,10 @@
 /*
 a. Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é 
-F ← 9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
+F ← (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
 */
+
+let celsius = parseFloat(prompt("Digite a temperatura em Celcius"));
+
+let fahrenheit = ((9 * celsius) + 160) / 5;
+
+alert(`A temperatura em Celsius ${celsius} convertida em Fahrenheit é ${fahrenheit}`);
--- Atividade-03/b.js
+++ Atividade-03/b.js
@@ -2,3 +2,9 @@
 b. Ler uma temperatura em graus Fahrenheit e apresentá-la convertida em graus Celsius. A fórmula de
 conversão é C ← (F -32)* (5/9), sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
 */
+
+let fahrenheit = parseFloat(prompt("Digite a temperatura em Fahrenheit"));
+
+let celsius = (fahrenheit - 32) * (5/9) ;
+
+alert(`A temperatura em Fahrenheit ${fahrenheit} convertida em Celsius é ${celsius}`);
