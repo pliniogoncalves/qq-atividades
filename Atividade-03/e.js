809e2a27824e3f3385dc73c60ca08d0ca191835e
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-16 18:15:46 -0300

    atividade 03 - questao e

--- Atividade-03/e.js
+++ Atividade-03/e.js
@@ -2,10 +2,13 @@
 e. Efetuar o cálculo e a apresentação do valor de uma prestação em atraso, utilizando a fórmula PRESTAÇÃO ← VALOR + (VALOR * (TAXA/100) * TEMPO). 
 */
 
-let valor = parseFloat(prompt("Digite o valor"));
-let taxa = parseFloat(prompt("Digite o valor da taxa"));
-let tempo = parseInt(prompt("Digite o valor do tempo"));
+function calculoPrestacao(){
+    let valor = parseFloat(document.getElementById("valor").value);
+    let taxa = parseFloat(document.getElementById("taxa").value);
+    let tempo = parseInt(document.getElementById("tempo").value);
 
-let prestacao = valor + (valor * (taxa/100) * tempo);
+    let prestacao = valor + (valor * (taxa/100) * tempo);
 
-alert(`O valor da prestação em atraso é R$${prestacao}`);
+    document.getElementById("resultado").innerText = `O valor da prestação em atraso é R$${prestacao}`;
+}
+
