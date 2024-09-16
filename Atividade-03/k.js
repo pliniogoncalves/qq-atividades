62b7c0e1bb49a1f269c205e956e209689a4556f0
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-16 19:54:09 -0300

    atividade 03 - questao k

--- Atividade-03/k.js
+++ Atividade-03/k.js
@@ -3,9 +3,11 @@ k. Elaborar um programa que efetue a apresentação do valor da conversão em re
 (US$). O programa deve solicitar o valor da cotação do dólar e também a quantidade de dólares disponível com o usuário. 
 */
 
-let dolar = parseFloat(prompt("Digite o valor que você possui em dólar(US$)"));
-let cotacao = parseFloat(prompt("Digite o valor da cotaçao do dólar em reais(R$)"));
+function converter(){
+    let dolar = parseFloat(document.getElementById("dolar").value);
+    let cotacao = parseFloat(document.getElementById("cotacao").value);
 
-let real = dolar * cotacao;
+    let real = dolar * cotacao;
 
-alert(`voce tem ${dolar} dolares e em real você tem ${real}`);
+    document.getElementById("resultado").innerText = `Você tem US$${dolar} dolares, e em real você tem R$${real}`;
+}
