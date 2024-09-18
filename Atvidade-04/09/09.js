3c1bc2b90193e9f1199f0eaa7d2f48bbd3b0fcaf
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-18 11:43:25 -0300

    atividade 04 - questao 09

--- Atvidade-04/09/09.js
+++ Atvidade-04/09/09.js
@@ -5,3 +5,27 @@ para homens: (72.7 * h) – 58;
 para mulheres: (62.1 * h) – 44.7. 
 */
 
+function calcularPeso(){
+    let sexo = document.getElementById("sexo").value.toUpperCase();
+    let altura = parseFloat(document.getElementById("altura").value);
+
+    if(altura<=0){
+        document.getElementById("resultado").innerText = `Digite uma altura válida`;
+        return;
+    }
+
+    if((sexo != "M") && (sexo != "F")){
+        document.getElementById("resultado").innerText = `Digite um sexo válido`;
+        return;
+    }
+
+    if(sexo == "M"){
+        let peso = (72.7 * altura) - 58;
+        document.getElementById("resultado").innerText = `Seu peso ideal é ${peso}`;
+    }else{
+        let peso = (62.1 * altura) - 44.7;
+        document.getElementById("resultado").innerText = `Seu peso ideal é ${peso}`;
+    }
+
+    
+}
