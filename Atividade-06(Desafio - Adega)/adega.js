ddfca4b5da98d38aed9d48289e128866af7b067a
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-23 09:56:06 -0300

    atividade 06 - desafio adega(estrutura base)

---
+++ Atividade-06(Desafio - Adega)/adega.js
@@ -0,0 +1,11 @@
+/*
+ Construa uma solução em JavaScript que permita fazer um levantamento do estoque de vinhos de uma adega. Os tipos de vinho são: 'T' para tinto, 'B' para branco e 'R' para rosé. A aplicação deve:
+
+Aceitar entradas dos tipos de vinho.
+Armazenar cada entrada em um array. 
+Exibir a lista atualizada de tipos de vinho e o total de cada tipo após cada entrada.
+Calcular e exibir a porcentagem de cada tipo sobre o total de vinhos inseridos.
+Permitir alterar o tipo de vinho armazenado em uma determinada posição do array.
+Finalizar a inserção de dados quando o usuário digitar 'F'.
+*/
+
