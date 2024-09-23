4823a3cc45e0d013ec680f0d78c85cdc7f56c28d
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-23 19:38:05 -0300

    atividade 06 - desafio adega - Inserir Vinhos)

--- Atividade-06(Desafio - Adega)/adega.js
+++ Atividade-06(Desafio - Adega)/adega.js
@@ -9,3 +9,69 @@ Permitir alterar o tipo de vinho armazenado em uma determinada posição do arra
 Finalizar a inserção de dados quando o usuário digitar 'F'.
 */
 
+let vinhos=[];
+
+function inserirVinhosMenu(){
+    vinhos = [];
+
+    const inserirVinhosContainer = document.getElementById("inserirVinhosContainer");
+    inserirVinhosContainer.innerHTML = '';
+
+    inserirVinhosContainer.innerHTML += `
+    <label for="vinhos">Digite a opção de Vinho que deseja Armazenar('T' para Tinto, 'B' para Branco e 'R' para Rosé). Se deseja finalizar, Digite "F":</label> 
+    <input type="text" id="vinhoInput">
+    `;
+
+    document.getElementById("inserirVinhos").style.display = 'block';
+}
+
+function armazenarVinhos(){
+    const vinhoInput = document.getElementById("vinhoInput").value.toUpperCase();
+
+    switch(true){
+        case(vinhoInput == 'T'):
+            vinhos.push('Tinto');
+            console.log(vinhos);
+            alert(`Vinho ${vinhoInput} adicionado.`)
+            document.getElementById('vinhoInput').value = '';
+            break;
+        case(vinhoInput == 'B'):
+            vinhos.push('Branco');
+            console.log(vinhos);
+            alert(`Vinho ${vinhoInput} adicionado.`)
+            document.getElementById('vinhoInput').value = '';
+            break;
+        case(vinhoInput == 'R'):
+            vinhos.push('Rosé');
+            console.log(vinhos);
+            alert(`Vinho ${vinhoInput} adicionado.`)
+            document.getElementById('vinhoInput').value = '';
+            break;
+        case(vinhoInput == 'F'):
+            finalizarInsercao();
+            break;
+        default:
+            alert('Opção Inválida! Use "T" para tinto, "B" para branco e "R" para rosé. Se deseja finalizar, Digite "F".');
+    }
+
+    /* 
+    if(vinhoInput == 'T' || vinhoInput == 'B' || vinhoInput == 'R'){
+        vinhos.push(vinhoInput);
+        console.log(vinhos);
+        alert(`Vinho ${vinhoInput} adicionado.`)
+        document.getElementById('vinhoInput').value = '';
+    }else if(vinhoInput == 'F'){
+        finalizarInsercao();
+    }else{
+        alert('Opção Inválida! Use "T" para tinto, "B" para branco e "R" para rosé. ')
+    }
+*/
+
+}
+
+function finalizarInsercao(){
+    alert('Inserção de vinhos finalizada!');
+    document.getElementById("inserirVinhos").style.display = 'none';
+    console.log(vinhos);
+}
+
