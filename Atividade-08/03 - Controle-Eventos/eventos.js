9dd1ebdb45ec21041f39e2afb2a9e0404e22094a
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-24 20:52:27 -0300

    atividade 08 - registro de eventos - finalizado

--- Atividade-08/03 - Controle-Eventos/eventos.js
+++ Atividade-08/03 - Controle-Eventos/eventos.js
@@ -10,95 +10,95 @@ conferências. A aplicação deve:
 . Finalizar a inserção de dados quando o usuário digitar 'F'.
 */
 
-let frutas = JSON.parse(localStorage.getItem('frutas')) || [];
+let eventos = JSON.parse(localStorage.getItem('eventos')) || [];
 
 listar();
 contar();
 calcularPorcentagem();
 
 function cadastrar(){
-    const novaFruta = document.getElementById('novaFruta').value.toUpperCase();
-    if(novaFruta == 'M' || novaFruta == 'B' || novaFruta == 'L'){
-        frutas.push(novaFruta);
-        localStorage.setItem('frutas', JSON.stringify(frutas));
-        alert(`${novaFruta} adicionado com sucesso!`);
-    }else if(novaFruta == 'F'){
-        document.getElementById('cadastroFrutas').style.display = 'none';
-        document.getElementById('alterarFrutas').style.display = 'none';
+    const novoEvento = document.getElementById('novoEvento').value.toUpperCase();
+    if(novoEvento == 'S' || novoEvento == 'W' || novoEvento == 'C'){
+        eventos.push(novoEvento);
+        localStorage.setItem('eventos', JSON.stringify(eventos));
+        alert(`${novoEvento} adicionado com sucesso!`);
+    }else if(novoEvento == 'F'){
+        document.getElementById('cadastroEventos').style.display = 'none';
+        document.getElementById('alterarEventos').style.display = 'none';
     }else{
-        alert(`${novaFruta} é uma entrada inválida!`)
+        alert(`${novoEvento} é uma entrada inválida!`)
     }
 
     listar();
     contar();
     calcularPorcentagem();
 
-    document.getElementById('novaFruta').value = '';
+    document.getElementById('novoEvento').value = '';
 }
 
 function listar(){
-    const listarFrutasContainer = document.getElementById('listarFrutasContainer');
-    listarFrutasContainer.innerHTML = '';
+    const listarEventosContainer = document.getElementById('listarEventosContainer');
+    listarEventosContainer.innerHTML = '';
 
-    frutas.forEach((fruta, index) => {
-        listarFrutasContainer.innerHTML += `
-            ${index + 1}. ${fruta} <br>
+    eventos.forEach((evento, index) => {
+        listarEventosContainer.innerHTML += `
+            ${index + 1}. ${evento} <br>
         `;
     });
 }
 
 function contar() {
-    const contarFrutasContainer = document.getElementById('contarFrutasContainer');
-    contarFrutasContainer.innerHTML = '';
+    const contarEventosContainer = document.getElementById('contarEventosContainer');
+    contarEventosContainer.innerHTML = '';
 
-    const contagem = frutas.reduce((contador, fruta) => {
-        contador[fruta] = (contador[fruta] || 0) + 1;
+    const contagem = eventos.reduce((contador, evento) => {
+        contador[evento] = (contador[evento] || 0) + 1;
         return contador;
     }, {});
 
-    for (const [fruta, quantidade] of Object.entries(contagem)) {
-        contarFrutasContainer.innerHTML += `${fruta}: ${quantidade} <br>`;
+    for (const [evento, quantidade] of Object.entries(contagem)) {
+        contarEventosContainer.innerHTML += `${evento}: ${quantidade} <br>`;
     }
 
     return contagem;
 }
 
 function calcularPorcentagem(){
-    const porcentagemFrutasContainer = document.getElementById('porcentagemFrutasContainer');
-    porcentagemFrutasContainer.innerHTML = '';
+    const porcentagemEventosContainer = document.getElementById('porcentagemEventosContainer');
+    porcentagemEventosContainer.innerHTML = '';
 
     const contagem = contar();
 
-    for(const [fruta, quantidade] of Object.entries(contagem)){
-        const porcentagem = (quantidade * 100) / frutas.length;
-        porcentagemFrutasContainer.innerHTML += `
-            ${fruta}: ${porcentagem.toFixed(2)}% <br>
+    for(const [evento, quantidade] of Object.entries(contagem)){
+        const porcentagem = (quantidade * 100) / eventos.length;
+        porcentagemEventosContainer.innerHTML += `
+            ${evento}: ${porcentagem.toFixed(2)}% <br>
         `;
     }
 }
 
 function alterar(){
-    const posicaoFruta = document.getElementById("posicaoFruta").value;
-    const frutaSubstituta = document.getElementById("frutaSubstituta").value.toUpperCase();
+    const posicaoEvento = document.getElementById("posicaoEvento").value;
+    const eventoSubstituto = document.getElementById("eventoSubstituto").value.toUpperCase();
 
-    if(!isNaN(posicaoFruta) && posicaoFruta >= 1 && posicaoFruta <= frutas.length){
-        if(frutaSubstituta == 'M' || frutaSubstituta == 'B' || frutaSubstituta == 'L'){
-            frutas[posicaoFruta-1] = frutaSubstituta;
-            localStorage.setItem('frutas', JSON.stringify(frutas));
-            alert(`${frutaSubstituta} adicionado com sucesso!`);
-        }else if(frutaSubstituta == 'F'){
-            document.getElementById('cadastroFrutas').style.display = 'none';
-            document.getElementById('alterarFrutas').style.display = 'none';
+    if(!isNaN(posicaoEvento) && posicaoEvento >= 1 && posicaoEvento <= eventos.length){
+        if(eventoSubstituto == 'S' || eventoSubstituto == 'W' || eventoSubstituto == 'C'){
+            eventos[posicaoEvento-1] = eventoSubstituto;
+            localStorage.setItem('eventos', JSON.stringify(eventos));
+            alert(`${eventoSubstituto} adicionado com sucesso!`);
+        }else if(eventoSubstituto == 'F'){
+            document.getElementById('cadastroEventos').style.display = 'none';
+            document.getElementById('alterarEventos').style.display = 'none';
         }else{
-            alert(`${frutaSubstituta} é uma entrada inválida!`)
+            alert(`${eventoSubstituto} é uma entrada inválida!`)
         }
     }else{
-        alert(`${posicaoFruta} é uma entrada inválida!`)
+        alert(`${posicaoEvento} é uma entrada inválida!`)
     }
 
     listar();
     contar();
     calcularPorcentagem();
 
-    document.getElementById('frutaSubstituta').value = '';
+    document.getElementById('eventoSubstituto').value = '';
 }
