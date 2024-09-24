3f8f636f293ca43d5240a3098d09e0ae3526d6ba
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-24 18:58:02 -0300

    atividade 08 - estoque de frutas - projeto base

---
+++ Atividade-08/Estoque-Frutas/frutas.js
@@ -0,0 +1,11 @@
+/*
+Construa uma solução em HTML e JavaScript que permita fazer um levantamento do estoque de 
+frutas em um supermercado. As frutas são: 'M' para maçã, 'B' para banana e 'L' para laranja. A 
+aplicação deve: 
+. Aceitar entradas das frutas disponíveis. 
+. Armazenar cada entrada em um array. 
+. Exibir a lista atualizada de frutas e o total de cada tipo após cada entrada. 
+. Calcular e exibir a porcentagem de cada fruta sobre o total de frutas inseridas. 
+. Permitir alterar a fruta armazenada em uma determinada posição do array. 
+. Finalizar a inserção de dados quando o usuário digitar 'F'.
+*/
