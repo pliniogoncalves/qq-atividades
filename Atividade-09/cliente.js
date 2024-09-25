266b4eb4ad91268f600c9b3a72fb8563b6c85582
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-25 18:48:36 -0300

    atividade 09 - loja ABC- cadastro cliente

--- Atividade-09/cliente.js
+++ Atividade-09/cliente.js
@@ -10,13 +10,45 @@ function formCadastrarCliente(){
             
             <form id="clienteForm">
 
-                <label for="nomeCliente">Nome: </label><br>
-                <input type="text" id="nomeCliente" required><br>
+                <label for="nomeCliente">Nome: </label>
+                <input type="text" id="nomeCliente" required><br><br>
 
-                <button type="button" onclick="iniciarCadastro()">Iniciar Cadastro</button>
+                <label for="dataCliente">Data de Nascimento: </label>
+                <input type="date" id="dataCliente" required><br><br>
+
+                <label for="cpfCliente">CPF: </label>
+                <input type="text" id="cpfCliente" required><br><br>
+
+                <label>Origem:</label>
+                <input type="radio" id="clienteOrigemLoja" name="origem" value="loja" required>
+                <label for="clienteOrigemLoja">Loja</label>
+                <input type="radio" id="clienteOrigemSite" name="origem" value="site">
+                <label for="clienteOrigemSite">Site</label><br><br>
+
+                <label for="scoreCliente">Score: </label>
+                <input type="number" id="scoreCliente" required><br><br>
+
+                <button type="button" onclick="cadastrarCliente()">Cadastrar</button>
 
             </form>
         `;
+}
+
+function cadastrarCliente(){
+    const nomeCliente = document.getElementById('nomeCliente').value;
+    const dataCliente = document.getElementById('dataCliente').value;
+    const cpfCliente = document.getElementById('cpfCliente').value;
+    const origemCliente = document.querySelector('input[name="origem"]:checked').value;
+    const scoreCliente = document.getElementById('scoreCliente').value;
+
+    const cliente = {nomeCliente, dataCliente, cpfCliente, origemCliente, scoreCliente};
+    clientes.push(cliente);
+
+    document.getElementById('clienteForm').reset();
+
+    console.log(clientes);
+
+    alert(`Cliente ${cliente.nomeCliente} Cadastrado com Sucesso!`);
 }
 
 function listar(){
