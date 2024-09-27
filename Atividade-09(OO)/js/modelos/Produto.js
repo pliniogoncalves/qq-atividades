4f506d48b2a2275bf0c5727e032651f39610b083
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-09-27 13:07:56 -0300

    atividade 09(POO) - loja ABC - classe produto

--- Atividade-09(OO)/js/modelos/Produto.js
+++ Atividade-09(OO)/js/modelos/Produto.js
@@ -6,4 +6,37 @@ export class Produto{
         this.valor = valor;
         this.categoria = categoria;
     }
+
+    salvarProduto() {
+        let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+        produtos.push(this);
+        localStorage.setItem('produtos', JSON.stringify(produtos));
+
+        return(`Produto ${this.nome} cadastrado com sucesso!`);
+    }
+
+    static listarProduto() {
+        const produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+        return produtos;
+    }
+
+    static editarProduto(index, produtoAtualizado) {
+        let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+        if (index >= 0 && index < produtos.length) {
+            produtos[index] = produtoAtualizado;
+            localStorage.setItem('produtos', JSON.stringify(produtos));
+            return(`Produto ${produtoAtualizado.nome} editado com sucesso!`);
+        }
+    }
+
+    static removerProduto(index) {
+        let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+        if (index >= 0 && index < produtos.length) {
+            produtos.splice(index, 1);
+            localStorage.setItem('produtos', JSON.stringify(produtos));
+            return('Produto removido com sucesso!');
+        } else {
+            return('ID de Produto inválido!');
+        }
+    }
 }
